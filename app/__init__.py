4d1e7b6b1a0438be453f2fa2fa2639f5af3a9f81
warranmav <warran@wozland.co.uk>
2024-09-27 13:59:16 +0100

    Added authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,29 @@
 from flask import Flask
-from .models import db
+from flask_login import LoginManager
+from .models import db, User
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sundayplanner.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     with app.app_context():
         db.create_all()
 
     from .routes import main
+    from .auth import auth as auth_blueprint
     app.register_blueprint(main)
+    app.register_blueprint(auth_blueprint)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required
 from .models import db, Speaker
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
 @main.route('/add_speaker', methods=['GET', 'POST'])
+@login_required
 def add_speaker():
     if request.method == 'POST':
         name = request.form['name']
